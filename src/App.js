a3577eae2ba5a54475765e3bc754f8f9e089fd2d
Marcelo Heinichen <romarhegu@gmail.com>
2019-08-10 12:26:35 -0400

    Primera version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const Frase = ({ frase }) =>
+  (
+    <div className="frase">
+      <h1>
+        {frase.quote}
+      </h1>
+      <p>{frase.author}</p>
     </div>
-  );
+  )
+
+const App = () => {
+
+  const [frase, setFrase] = useState({});
+
+
+  const consulta = async () => {
+
+    const resultado = await axios('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
+
+    //console.log(resultado.data[0]);
+
+    setFrase(resultado.data[0]);
+  }
+
+  /* const fet = () => {
+    fetch(`data.json`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then(res => console.log(res))
+  } */
+  //consulta a rest api
+  useEffect(
+    () => {
+      consulta();
+      //fet();
+    }, []
+
+  )
+
+  return (
+    <div className="contenedor">
+      <Frase
+        frase={frase}
+      />
+      <button
+        onClick={consulta}
+      >Generar Nueva Frase</button>
+    </div>
+  )
 }
 
 export default App;
